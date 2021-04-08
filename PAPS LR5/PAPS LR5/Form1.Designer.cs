92e6105a7d43da08cfa03921cbdf35851f490d9d
OlyaOrli <olya_orel@inbox.ru>
2021-04-08 10:18:19 +0300

    change readme

---
+++ PAPS LR5/PAPS LR5/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace PAPS_LR5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.first = new System.Windows.Forms.TextBox();
+            this.division = new System.Windows.Forms.Button();
+            this.multiplication = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.second = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // first
+            // 
+            this.first.Location = new System.Drawing.Point(237, 127);
+            this.first.Name = "first";
+            this.first.Size = new System.Drawing.Size(204, 22);
+            this.first.TabIndex = 0;
+            // 
+            // division
+            // 
+            this.division.Location = new System.Drawing.Point(379, 320);
+            this.division.Name = "division";
+            this.division.Size = new System.Drawing.Size(62, 60);
+            this.division.TabIndex = 13;
+            this.division.Text = "/";
+            this.division.UseVisualStyleBackColor = true;
+            this.division.Click += new System.EventHandler(this.division_Click);
+            // 
+            // multiplication
+            // 
+            this.multiplication.Location = new System.Drawing.Point(282, 320);
+            this.multiplication.Name = "multiplication";
+            this.multiplication.Size = new System.Drawing.Size(62, 60);
+            this.multiplication.TabIndex = 17;
+            this.multiplication.Text = "*";
+            this.multiplication.UseVisualStyleBackColor = true;
+            this.multiplication.Click += new System.EventHandler(this.multiplication_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(185, 320);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(62, 60);
+            this.minus.TabIndex = 18;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(88, 320);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(62, 60);
+            this.add.TabIndex = 19;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Первое число";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 17);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Второе число";
+            // 
+            // second
+            // 
+            this.second.Location = new System.Drawing.Point(237, 215);
+            this.second.Name = "second";
+            this.second.Size = new System.Drawing.Size(204, 22);
+            this.second.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 521);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(341, 17);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "ВНИМАНИЕ! Результат вычислений - целое число";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(172, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 17);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Действия с числами";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(76, 383);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 17);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Сложение";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(165, 383);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 17);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Вычитание";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(374, 383);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 17);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "Деление";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(263, 383);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 17);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Умножение";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(2, 56);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(516, 17);
+            this.label9.TabIndex = 29;
+            this.label9.Text = "ИНСТРУКЦИЯ! Введитие два целых числа, после выбирите действие с ними";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(217, 455);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(76, 17);
+            this.result.TabIndex = 30;
+            this.result.Text = "Результат";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 573);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.second);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.multiplication);
+            this.Controls.Add(this.division);
+            this.Controls.Add(this.first);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox first;
+        private System.Windows.Forms.Button division;
+        private System.Windows.Forms.Button multiplication;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox second;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ PAPS LR5/PAPS LR5/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Используемый веб-сервис работает с двумя целыми числами и выполняет следующие функции:
+// сложение, вычитание, умножение, деление.
+
+namespace PAPS_LR5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static int number1, number2;
+        bool f=false, f1=false;
+
+        //Веб-сервис -  калькулятор, создаем и подключаем клиента
+        ServiceReference1.CalculatorSoap calculator = new ServiceReference1.CalculatorSoapClient();
+        
+        
+        // функция для считывыния данных. Проверка на пустоту полей и верный формат введенных чисел.
+        public void data()
+        {
+            if ((first.Text == "") && (second.Text == "")) MessageBox.Show("Сначала введите числа", "Предупреждение");
+            else if (first.Text == "") MessageBox.Show("Введите первое число", "Предупреждение");
+            else if (second.Text == "") MessageBox.Show("Введите второе число", "Предупреждение");
+            else
+            {
+                try
+                {
+                    number1 = Convert.ToInt32(first.Text);
+                    f = true;
+                }
+                catch (Exception ex)
+                {
+                    first.ResetText();
+                    MessageBox.Show("Неверный формат первого числа.\nВведите целое число", "Предупреждение");
+                }
+                try
+                {
+                    number2 = Convert.ToInt32(second.Text);
+                    f1 = true;
+                }
+                catch (Exception ex)
+                {
+                    second.ResetText();
+                    MessageBox.Show("Неверный формат второго числа.\nВведите целое число", "Предупреждение");
+                }
+                
+            }
+        }
+
+        // метод отвечающий за сложение. Запуск функции считывыния данных, отправка чисел на сервер, получение ответа и отображение
+        private void add_Click(object sender, EventArgs e)
+        {
+            data();
+            if (f && f1)
+            {
+                String str = "Результат: " + calculator.Add(number1, number2).ToString();
+                result.Text = str;
+            }
+        }
+
+        // метод отвечающий за вычитание. Запуск функции считывыния данных, отправка чисел на сервер, получение ответа и отображение
+        private void minus_Click(object sender, EventArgs e)
+        {
+            data();
+            if (f && f1)
+            {
+                String str = "Результат: " + calculator.Subtract(number1, number2).ToString();
+                result.Text = str;
+            }
+        }
+
+        // метод отвечающий за умножение. Запуск функции считывыния данных, отправка чисел на сервер, получение ответа и отображение
+        private void multiplication_Click(object sender, EventArgs e)
+        {
+            data();
+            if (f && f1)
+            {
+                String str = "Результат: " + calculator.Multiply(number1, number2).ToString();
+                result.Text = str;
+            }
+        }
+
+        // метод отвечающий за деление. Запуск функции считывыния данных, отправка чисел на сервер, получение ответа и отображение
+        private void division_Click(object sender, EventArgs e)
+        {
+            data();
+            if (f && f1)
+            {
+                if (number2 == 0)
+                {
+                    result.Text = "Результат: Ошибка. На 0 делить нельзя";
+
+                }
+                else
+                {
+                    String str = "Результат: " + calculator.Divide(number1, number2).ToString();
+                    result.Text = str;
+                }
+            }
+        }
+    }
+}
