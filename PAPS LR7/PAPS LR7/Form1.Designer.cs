7d36e9f50d29a3c0f536f85820b8b4f1402ba2ea
OlyaOrli <olya_orel@inbox.ru>
2021-04-13 22:50:20 +0300

    change readme

---
+++ PAPS LR7/PAPS LR7/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace PAPS_LR7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.predmet = new System.Windows.Forms.TextBox();
+            this.number_grup = new System.Windows.Forms.TextBox();
+            this.fio_student = new System.Windows.Forms.TextBox();
+            this.number = new System.Windows.Forms.TextBox();
+            this.tema = new System.Windows.Forms.TextBox();
+            this.name_specialty = new System.Windows.Forms.TextBox();
+            this.number_specialty = new System.Windows.Forms.TextBox();
+            this.name_kafedra = new System.Windows.Forms.TextBox();
+            this.fio_teacher = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.Button();
+            this.year = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 267);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Фамилия И.О.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Данные о студенте";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(217, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(223, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Данные о лабораторной работе";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(261, 432);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Данные о преподавателе";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(173, 305);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Группа";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(73, 343);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(150, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Шифр специальности";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(52, 380);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(176, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Название специальности";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(125, 467);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(103, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Фамилия И.О.";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(92, 505);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(136, 17);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Название кафедры";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(71, 71);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(157, 17);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Название дисциплины";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(186, 105);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(42, 17);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Тема";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(177, 139);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(51, 17);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Номер";
+            // 
+            // predmet
+            // 
+            this.predmet.Location = new System.Drawing.Point(291, 66);
+            this.predmet.Name = "predmet";
+            this.predmet.Size = new System.Drawing.Size(407, 22);
+            this.predmet.TabIndex = 12;
+            // 
+            // number_grup
+            // 
+            this.number_grup.Location = new System.Drawing.Point(291, 302);
+            this.number_grup.Name = "number_grup";
+            this.number_grup.Size = new System.Drawing.Size(407, 22);
+            this.number_grup.TabIndex = 13;
+            // 
+            // fio_student
+            // 
+            this.fio_student.Location = new System.Drawing.Point(291, 264);
+            this.fio_student.Name = "fio_student";
+            this.fio_student.Size = new System.Drawing.Size(407, 22);
+            this.fio_student.TabIndex = 14;
+            // 
+            // number
+            // 
+            this.number.Location = new System.Drawing.Point(291, 136);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(407, 22);
+            this.number.TabIndex = 15;
+            // 
+            // tema
+            // 
+            this.tema.Location = new System.Drawing.Point(291, 102);
+            this.tema.Name = "tema";
+            this.tema.Size = new System.Drawing.Size(407, 22);
+            this.tema.TabIndex = 16;
+            // 
+            // name_specialty
+            // 
+            this.name_specialty.Location = new System.Drawing.Point(291, 377);
+            this.name_specialty.Name = "name_specialty";
+            this.name_specialty.Size = new System.Drawing.Size(407, 22);
+            this.name_specialty.TabIndex = 17;
+            // 
+            // number_specialty
+            // 
+            this.number_specialty.Location = new System.Drawing.Point(291, 340);
+            this.number_specialty.Name = "number_specialty";
+            this.number_specialty.Size = new System.Drawing.Size(407, 22);
+            this.number_specialty.TabIndex = 18;
+            // 
+            // name_kafedra
+            // 
+            this.name_kafedra.Location = new System.Drawing.Point(291, 502);
+            this.name_kafedra.Name = "name_kafedra";
+            this.name_kafedra.Size = new System.Drawing.Size(407, 22);
+            this.name_kafedra.TabIndex = 19;
+            // 
+            // fio_teacher
+            // 
+            this.fio_teacher.Location = new System.Drawing.Point(291, 464);
+            this.fio_teacher.Name = "fio_teacher";
+            this.fio_teacher.Size = new System.Drawing.Size(407, 22);
+            this.fio_teacher.TabIndex = 20;
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(291, 560);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(93, 31);
+            this.result.TabIndex = 21;
+            this.result.Text = "Создать";
+            this.result.UseVisualStyleBackColor = true;
+            this.result.Click += new System.EventHandler(this.result_Click);
+            // 
+            // year
+            // 
+            this.year.Location = new System.Drawing.Point(291, 174);
+            this.year.Name = "year";
+            this.year.Size = new System.Drawing.Size(407, 22);
+            this.year.TabIndex = 22;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(196, 177);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(32, 17);
+            this.label13.TabIndex = 23;
+            this.label13.Text = "Год";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 615);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.year);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.fio_teacher);
+            this.Controls.Add(this.name_kafedra);
+            this.Controls.Add(this.number_specialty);
+            this.Controls.Add(this.name_specialty);
+            this.Controls.Add(this.tema);
+            this.Controls.Add(this.number);
+            this.Controls.Add(this.fio_student);
+            this.Controls.Add(this.number_grup);
+            this.Controls.Add(this.predmet);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox predmet;
+        private System.Windows.Forms.TextBox number_grup;
+        private System.Windows.Forms.TextBox fio_student;
+        private System.Windows.Forms.TextBox number;
+        private System.Windows.Forms.TextBox tema;
+        private System.Windows.Forms.TextBox name_specialty;
+        private System.Windows.Forms.TextBox number_specialty;
+        private System.Windows.Forms.TextBox name_kafedra;
+        private System.Windows.Forms.TextBox fio_teacher;
+        private System.Windows.Forms.Button result;
+        private System.Windows.Forms.TextBox year;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ PAPS LR7/PAPS LR7/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace PAPS_LR7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // функция для проверки введенных чисел
+        public int data_int(TextBox text)
+        {
+            int k = -1;
+            try
+            {
+                k = Convert.ToInt32(text.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Неверный формат числа", "Предупреждение");
+                text.ResetText();
+            }
+            return k;
+        }
+
+        // функция создания файла
+        public void text(String kafedra, String number_lr, String tema, String predmet, String avtor, String speciality,
+            String grup, String teacher, String year)
+        {
+            // Получить объект приложения Word.
+            Word.Application word_app = new Word.Application();
+            word_app.Visible = true;
+
+            // Создаем документ Word.
+            object missing = Type.Missing;
+            Word.Document word_doc = word_app.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+            Word.Paragraph para = word_doc.Paragraphs.Last; // создаем параграф
+            //задаем стиль
+            para.Range.Font.Name = "Times New Roman";
+            para.Range.Font.Size = 14;
+            para.Range.Paragraphs.Space1();
+            para.Range.Paragraphs.SpaceAfter = 0;
+
+            para.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            
+            //сохраняем текст
+            para.Range.Text = "МИНИСТЕРСТВО НАУКИ И ВЫСШЕГО ОБРАЗОВАНИЯ\n" +
+                "РОССИЙСКОЙ ФЕДЕРАЦИИ\n" +
+            "ФЕДЕРАЛЬНОЕ ГОСУДАРСТВЕННОЕ БЮДЖЕТНОЕ ОБРАЗОВАТЕЛЬНОЕ УЧРЕЖДЕНИЕ ВЫСШЕГО ОБРАЗОВАНИЯ\n" +
+            "«ОРЛОВСКИЙ ГОСУДАРСТВЕННЫЙ УНИВЕРСИТЕТ\n" +
+            "ИМЕНИ И.С.ТУРГЕНЕВА»\n\n" +
+            "Кафедра "+kafedra+"\n\n\n\n\n\n";
+
+            para.Range.Text = "ОТЧЕТ";
+            para.Range.Bold = 2;
+            para.Range.InsertParagraphAfter();
+            para.Range.Bold = 0;
+
+            para.Range.Text = "По лабораторной работе №"+ number_lr+ "\n" +
+                "на тему: «"+tema+"»\n" +
+                "по дисциплине: «"+predmet+"»\n\n\n\n\n\n\n\n\n";
+
+            para.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+            para.Range.Text = "Выполнил: "+avtor+"\n" +
+                "Институт приборостроения, автоматизации и информационных технологий\n" +
+                "Направление: "+speciality+"\n" +
+                "Группа: "+grup+"\n" +
+                "Проверил: "+teacher+"\n\n\n";
+
+            para.Range.Text = "Отметка о зачете: \n";
+            
+
+            para.Alignment = Word.WdParagraphAlignment.wdAlignParagraphRight;
+            para.Range.Text = "Дата: «____» ____________ "+year+" г.\n";
+           
+
+            para.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            para.Range.Text = "\n\n\n\n\n\nОрел, "+year;
+            para.Range.InsertParagraphAfter();
+        }
+
+        // считываем данные введенные пользователем и проверяем поля на пустоту
+        private void result_Click(object sender, EventArgs e)
+        {
+            if ((predmet.Text == "") && (name_kafedra.Text == "") && (name_specialty.Text == "") && (number_grup.Text == "") && (number_specialty.Text == "")
+                && (number.Text == "") && (tema.Text == "") && (year.Text == "") && (fio_student.Text == "") && (fio_teacher.Text == ""))
+                MessageBox.Show("Заполните все поля", "Предупреждение");
+            else
+            {
+                String s_kafedra, s_number_lr, s_tema, s_predmet, s_avtor, s_grup, s_teacher, s_year, speciality;
+               
+                int n1 = data_int(number);
+                int n2 = data_int(year);
+                if ((n1>0)&&(n2>1980))
+                {
+                    s_kafedra = name_kafedra.Text;
+                    s_predmet = name_kafedra.Text;
+                    s_number_lr = number.Text;
+                    s_tema = tema.Text;
+                    s_predmet = predmet.Text;
+                    s_avtor = fio_student.Text;
+                    speciality = number_specialty.Text + " " + name_specialty.Text;
+                    s_grup = number_grup.Text;
+                    s_teacher = fio_teacher.Text;
+                    s_year = year.Text;
+                    text(s_kafedra, s_number_lr, s_tema, s_predmet, s_avtor, speciality, s_grup, s_teacher, s_year);
+                }
+                
+            }
+
+
+        }
+    }
+}
